5d1bb6efd1edd377b9d0e51e4387b4600893ebc3
arunkumar0398 <32316065+arunkumar0398@users.noreply.github.com>
2024-05-03 23:09:30 +0530

    added assets and routes for login & dashboard page

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+
+export const Dashboard = () => {
+    return (
+        <>
+            <h1>dashboard page</h1>
+        </>
+    );
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,8 @@
+
+export const Login = () => {
+    return (
+        <>
+            <h1>Login page</h1>
+        </>
+    );
+}
